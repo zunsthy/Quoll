9d9bbd5b8ecf4648bc15f2331953cde8ca1aa6e8
ZunSThy <zunsthy@gmail.com>
2015-02-01 13:45:59 +0800

    dailylog

--- class/history.class.php
+++ class/history.class.php
@@ -13,28 +13,52 @@ class HISTORY {
 			case 'normal':
 				$where .= "'normal'";
 				break;
+			case 'all':
 			default:
 				$where = "";
 				break;
 		}
-		// d("SELECT COUNT(*) FROM sitelog " .$where);
-		$res = Q::$DB->q("SELECT COUNT(*) FROM sitelog " .$where);
-		$row = $res->fetch_row();
-		$count = 0 + $row[0];
-		// d($count);
-		if($count == 0)
-			return array();
 		
 		$field = "added, txt, security_level AS level";
 		$orderby = "added DESC";
 		$per = 50;
-		// d($orderby);
-		list($ps, $p, $limit) = UTILITY::page(50, $count);
-		// d($limit);
-		$res = Q::$DB->q("SELECT $field FROM sitelog $where ORDER BY $orderby $limit");
-		for($ret = []; $row = $res->fetch_assoc(); $ret[] = $row)
-			;
-		return $ret;
+		return UTILITY::query($field, "sitelog", $where, $orderby, $per);
+	}
+	
+	static public function searchLog($str, $level = 'normal'){
+		$wherea = array();
+		switch($level){
+			case 'mod': 
+				$wherea[] = "security_level = 'mod'";
+				break;
+			case 'normal':
+				$wherea[] = "security_level = 'normal'";
+				break;
+			case 'all':
+			default:
+				break;
+		}
+		$str = Q::$DB->real_escape_string($str);
+		$str = str_replace([ '+', ',', '.' ], ' ', $str);
+		$str = preg_replace('/\s\s+', ' ', $str);
+		$strs = explode(' ', $str, 10);
+		if(empty($strs))
+			return array();
+		
+		foreach($strs as $s){
+			$wherea[] = "txt LIKE '%$s%'";
+		}
+		
+		$where = "WHERE ". implode(" AND ", $wherea);
+		$field = "added, txt, security_level AS level";
+		$orderby = "added DESC";
+		$per = 50;
+		return UTILITY::query($field, "sitelog", $where, $orderby, $per);
+	}
+	
+	static public function addLog($str, $level = 'normal'){
+		// maybe use (class)LOG::writeLog($str, $level)
+		return false;
 	}
 	
 	static public function listChronicle(){
